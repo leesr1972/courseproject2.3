fee87055dbd8c2905955c2c6d20e2c517b39e165
Ли Сергей <sergey.r.lee@gmail.com>
2022-05-28 14:00:30 +0300

    Комит к уровню Mastermind курсовой работы 2-го курса.

---
+++ src/test/java/sky/pro/java/course2/courseproject2_3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.courseproject2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
